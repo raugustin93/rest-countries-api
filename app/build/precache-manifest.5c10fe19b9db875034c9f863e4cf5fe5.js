a79c9e3bae89891b30eb2a22be6c6b52273fa21b
Rudson <rudson.augustin@yahoo.com>
2020-05-16 21:27:50 -0400

    gh-pages

---
+++ app/build/precache-manifest.5c10fe19b9db875034c9f863e4cf5fe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14ea4ea7446b94b18f84d4a6e902c9e",
+    "url": "/rest-countries-api/app/index.html"
+  },
+  {
+    "revision": "a08640253d15dbbe3eb6",
+    "url": "/rest-countries-api/app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "cad1047c14070571a6cf",
+    "url": "/rest-countries-api/app/static/css/main.28967646.chunk.css"
+  },
+  {
+    "revision": "a08640253d15dbbe3eb6",
+    "url": "/rest-countries-api/app/static/js/2.9b325814.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rest-countries-api/app/static/js/2.9b325814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cad1047c14070571a6cf",
+    "url": "/rest-countries-api/app/static/js/main.e76a360b.chunk.js"
+  },
+  {
+    "revision": "a9dd78fa03c0d0958608",
+    "url": "/rest-countries-api/app/static/js/runtime-main.09658462.js"
+  }
+]);
