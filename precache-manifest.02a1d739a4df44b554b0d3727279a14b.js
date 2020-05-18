bb9ac584582f2340e2f663cbff1c1fcd540952db
Rudson <rudson.augustin@yahoo.com>
2020-05-18 19:22:53 -0400

    Updates

---
+++ precache-manifest.02a1d739a4df44b554b0d3727279a14b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2145d97ab100ff5455c3e21a63b1ccd1",
+    "url": "/rest-countries-api/index.html"
+  },
+  {
+    "revision": "6b99c3d346145db1d7b5",
+    "url": "/rest-countries-api/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c5e47ab61bb488c8ccef",
+    "url": "/rest-countries-api/static/css/main.28967646.chunk.css"
+  },
+  {
+    "revision": "6b99c3d346145db1d7b5",
+    "url": "/rest-countries-api/static/js/2.427fb32b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rest-countries-api/static/js/2.427fb32b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5e47ab61bb488c8ccef",
+    "url": "/rest-countries-api/static/js/main.53a9f4e4.chunk.js"
+  },
+  {
+    "revision": "fbd2ad259358bb1e7e2c",
+    "url": "/rest-countries-api/static/js/runtime-main.f71b98d8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.36e51cabe284c62cc10372daa377d219.js"
+  "/rest-countries-api/precache-manifest.02a1d739a4df44b554b0d3727279a14b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rest-countries-api/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
